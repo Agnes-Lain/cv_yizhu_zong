319bcdc2e04584f07bc18efaa101315e00626f2e
Yizhu ZONG <agnes.lain@gmail.com>
2020-10-02 00:37:47 +0200

    new deploy

---
+++ src/javascripts/init_exp_modal.js
@@ -0,0 +1,32 @@
+const initExpModal = () => {
+  // Get all the mental boxes
+  const modals = document.querySelectorAll(".exp-modal");
+
+  // Get all the buttons that opens the modal
+  const btns = document.querySelectorAll(".company-exp");
+
+  // Get all the <span> element that closes the modal
+  const spans = document.querySelectorAll(".exp-close");
+
+  // iterate the btns to trigger the event of each modal boxes
+  btns.forEach((btn, index) => {
+    const modal = modals[index];
+    const span = spans[index];
+    // When the user clicks on the button, open the modal
+    btn.onclick = () => {
+      modal.style.display = "block";
+    };
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = () => {
+      modal.style.display = "none";
+    };
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
+  });
+};
+
+export { initExpModal };
