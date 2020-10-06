51b191e5fed2254eab1d42ebcba0fc21346a4c5c
Yizhu ZONG <agnes.lain@gmail.com>
2020-10-06 02:44:58 +0200

    new deploy

--- src/javascripts/init_project_info.js
+++ src/javascripts/init_project_info.js
@@ -1,5 +1,19 @@
 const initProjectInfo = () => {
-
+  const array = [1, 2, 3];
+  array.forEach((i) => {
+    const project = document.getElementById(`project${i}`);
+    const projectInfo = document.getElementById(`project${i}-info`);
+    project.addEventListener("mouseover", (event) => {
+      projectInfo.style.display = "block";
+    });
+  });
+  array.forEach((i) => {
+    const project = document.getElementById(`project${i}`);
+    const projectInfo = document.getElementById(`project${i}-info`);
+    project.addEventListener("mouseleave", (event) => {
+      projectInfo.style.display = "none";
+    });
+  });
 };
 
 export { initProjectInfo };
