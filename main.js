7f9566ab2d2aba2a4eb9944bdbc267715a45d556
Yizhu ZONG <agnes.lain@gmail.com>
2020-10-26 19:53:02 +0100

    new deploy

--- main.js
+++ main.js
@@ -18328,23 +18328,32 @@ const initExpModal = () => {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "initMenu", function() { return initMenu; });
 const initMenu = () => {
-  const menueButton = document.querySelector(".toggle-holder");
+  const menuButton = document.querySelector(".toggle-holder");
   const slideMenu = document.querySelector(".slide-menu");
-  menueButton.addEventListener('click', (event) => {
-    if (slideMenu.classList.contains('menu-active')) {
-      slideMenu.classList.remove('menu-active');
-      menueButton.classList.remove('on');
-      // window.addEventListener("click", (e) => {
-      //   if (e.target !== slideMenu) {
-      //     slideMenu.classList.remove('menu-active');
-      //     menueButton.classList.remove('on');
-      //   }
-      // });
+  const toggle = document.getElementById("toggle")
+  window.addEventListener("click", (event) => {
+    if (event.target === menuButton || event.target.parentNode === toggle || event.target === toggle) {
+      if (slideMenu.classList.contains('menu-active')) {
+        slideMenu.classList.remove('menu-active');
+        menuButton.classList.remove('on');
+      } else {
+        slideMenu.classList.add('menu-active');
+        menuButton.classList.add('on');
+      }
     } else {
-      slideMenu.classList.add('menu-active');
-      menueButton.classList.add('on');
+      slideMenu.classList.remove('menu-active');
+      menuButton.classList.remove('on');
     }
   });
+  // menueButton.addEventListener('click', (event) => {
+  //   if (slideMenu.classList.contains('menu-active')) {
+  //     slideMenu.classList.remove('menu-active');
+  //     menueButton.classList.remove('on');
+  //   } else {
+  //     slideMenu.classList.add('menu-active');
+  //     menueButton.classList.add('on');
+  //   }
+  // });
 };
 
 
